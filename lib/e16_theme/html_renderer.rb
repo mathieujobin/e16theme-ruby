33309e257cdb1c89e908947922f692414af0634f
Mathieu Jobin <mathieu@justbudget.com>
2020-06-14 18:46:03 +0900

    ability to flip modes and load images from from web

--- lib/e16_theme/html_renderer.rb
+++ lib/e16_theme/html_renderer.rb
@@ -1,9 +1,10 @@
 module E16Theme
   class HtmlRenderer
-    def initialize(theme_path, image_definitions, border_definitions)
+    def initialize(theme_path, image_definitions, border_definitions, mode)
       @theme_path = theme_path
       @image_definitions = image_definitions
       @border_definitions = border_definitions
+      @mode = mode || found_modes.first
     end
 
     def draw_window_html
@@ -14,14 +15,15 @@ module E16Theme
       #{draw_element(:BAR_TOP)}
       #{draw_element(:TITLEBAR)}
       %Q{
-        #{found_modes}
-
         <style>
         td {border: 0px solid red;}
         td.inside {background-color: darkgreen; }
         td.left {text-align: right; }
         tr.bottom td {vertical-align: top; }
         </style>
+        <ul>
+          #{found_modes_links}
+        </ul>
         <table border=0 cellpadding=0 cellspacing=0>
           <tr class='top'>
             <td class='left'>
@@ -79,16 +81,22 @@ module E16Theme
       if @border_definitions[element].present?
         puts @border_definitions[element]
       end
-      img_link @image_definitions.dig(element, :clicked_pix)
+      img_link @image_definitions.dig(element, @mode.to_sym)
     end
 
     def img_link(link)
       image_path = "#{@theme_path}/#{link}"
-      "<img src='#{image_path}' />" if !link.to_s.empty? && File.exists?(image_path)
+      "<img src='#{image_path}' />" if !link.to_s.empty?
     end
 
     def found_modes
       @image_definitions.values.map {|v| v&.keys&.grep(/pix$/)}.flatten.uniq
+    end
+
+    def found_modes_links
+      found_modes.map do |mode|
+        "<li><a href=?mode=#{mode}>#{mode}</a></li>"
+      end.join("\n")
     end
   end
 end
